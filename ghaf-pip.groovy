4c25cb9dd6bf74a4a8b8469105d0632ffd296aa6
Karim Mdmirajul <143718491+karim20230@users.noreply.github.com>
2024-06-07 14:22:18 +0300

    Update ghaf-pip.groovy

--- ghaf-pip.groovy
+++ ghaf-pip.groovy
@@ -1,135 +1,116 @@
-#!/usr/bin/env groovy
+/*
+Copyright (c) 2018 Expedia Group.
+All rights reserved.  http://www.homeaway.com
 
-// SPDX-FileCopyrightText: 2024 Technology Innovation Institute (TII)
-//
-// SPDX-License-Identifier: Apache-2.0
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-pipeline {
-    agent any
-    parameters {
-        string(name: 'URL', defaultValue: 'https://github.com/tiiuae/ghaf.git', description: 'Repository URL')
-        string(name: 'BRANCH', defaultValue: 'main', description: 'Branch to build')
-    }
-    triggers {
-        pollSCM('* * * * *')
-    }
-    options {
-        timestamps()
-        disableConcurrentBuilds()
-        buildDiscarder logRotator(
-            artifactDaysToKeepStr: '7',
-            artifactNumToKeepStr: '10',
-            daysToKeepStr: '70',
-            numToKeepStr: '100'
-        )
-    }
-    stages {
-        stage('Checkout') {
-            steps {
-                dir('ghaf') {
-                    checkout([$class: 'GitSCM', 
-                        branches: [[name: params.BRANCH]], 
-                        doGenerateSubmoduleConfigurations: false, 
-                        extensions: [[$class: 'CleanCheckout']], 
-                        userRemoteConfigs: [[url: params.URL]]
-                    ])
-                }
-            }
-        }
-        stage('Build on x86_64') {
-            steps {
-                script {
-                    env.ts_build_begin = sh(script: 'date +%s', returnStdout: true).trim()
-                }
-                dir('ghaf') {
-                    sh 'nix build -L .#packages.x86_64-linux.nvidia-jetson-orin-agx-debug-from-x86_64 -o result-crosscompile-jetson-orin-agx-debug'
-                    sh 'nix build -L .#packages.x86_64-linux.nvidia-jetson-orin-nx-debug-from-x86_64  -o result-crosscompile-jetson-orin-nx-debug'
-                    sh 'nix build -L .#packages.x86_64-linux.generic-x86_64-debug                     -o result-generic-x86_64-debug'
-                    sh 'nix build -L .#packages.x86_64-linux.lenovo-x1-carbon-gen11-debug             -o result-lenovo-x1-carbon-gen11-debug'
-                    sh 'nix build -L .#packages.riscv64-linux.microchip-icicle-kit-debug              -o result-microchip-icicle-kit-debug'
-                    sh 'nix build -L .#packages.x86_64-linux.doc                                      -o result-doc'
-                }
-            }
-        }
-        stage('Build on aarch64') {
-            steps {
-                dir('ghaf') {
-                    sh 'nix build -L .#packages.aarch64-linux.nvidia-jetson-orin-agx-debug -o result-aarch64-jetson-orin-agx-debug'
-                    sh 'nix build -L .#packages.aarch64-linux.nvidia-jetson-orin-nx-debug  -o result-aarch64-jetson-orin-nx-debug'
-                    sh 'nix build -L .#packages.aarch64-linux.doc                          -o result-aarch64-doc'
-                }
-                script {
-                    env.ts_build_finished = sh(script: 'date +%s', returnStdout: true).trim()
-                }
-            }
-        }
-        stage('Provenance') {
-            environment {
-                // TODO: Write our own buildtype and builder id documents
-                PROVENANCE_BUILD_TYPE = "https://docs.cimon.build/provenance/buildtypes/jenkins/v1"
-                PROVENANCE_BUILDER_ID = "https://github.com/tiiuae/ghaf-infra/tree/main/terraform"
-                PROVENANCE_INVOCATION_ID = "${env.JOB_NAME}/${env.BUILD_ID}"
-                PROVENANCE_TIMESTAMP_BEGIN = "${env.ts_build_begin}"
-                PROVENANCE_TIMESTAMP_FINISHED = "${env.ts_build_finished}"
-                PROVENANCE_EXTERNAL_PARAMS = sh(
-                    returnStdout: true,
-                    script: 'jq -n --arg flakeURI $URL --arg flakeBranch $BRANCH \'$ARGS.named\''
-                ).trim()
-                PROVENANCE_INTERNAL_PARAMS = sh(
-                    returnStdout: true,
-                    // returns the specified environment variables in json format
-                    script: """
-                        jq -n env | jq "{ \
-                          JOB_NAME, \
-                          GIT_URL, \
-                          GIT_BRANCH, \
-                          GIT_COMMIT \
-                        }"
-                    """
-                ).trim()
-            }
-            steps {
-                dir('ghaf') {
-                    sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#provenance -- .#packages.x86_64-linux.nvidia-jetson-orin-agx-debug-from-x86_64 --recursive --out result-provenance-jetson-orin-agx-debug.json'
-                    sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#provenance -- .#packages.x86_64-linux.nvidia-jetson-orin-nx-debug-from-x86_64  --recursive --out result-provenance-jetson-orin-nx-debug.json'
-                    sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#provenance -- .#packages.x86_64-linux.generic-x86_64-debug                     --recursive --out result-provenance-generic-x86_64-debug.json'
-                    sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#provenance -- .#packages.x86_64-linux.lenovo-x1-carbon-gen11-debug             --recursive --out result-provenance-lenovo-x1-carbon-gen11-debug.json'
-                    sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#provenance -- .#packages.riscv64-linux.microchip-icicle-kit-debug              --recursive --out result-provenance-microchip-icicle-kit-debug.json'
-                    sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#provenance -- .#packages.aarch64-linux.nvidia-jetson-orin-agx-debug            --recursive --out result-provenance-aarch64-jetson-orin-agx-debug.json'
-                    sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#provenance -- .#packages.aarch64-linux.nvidia-jetson-orin-nx-debug             --recursive --out result-provenance-aarch64-jetson-orin-nx-debug.json'
-                }
-            }
-        }
-        stage('SBOM') {
-            steps {
-                dir('ghaf') {
-                    sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#sbomnix -- .#packages.x86_64-linux.nvidia-jetson-orin-agx-debug-from-x86_64 --csv result-sbom-crosscompile-jetson-orin-agx-debug.csv --cdx result-sbom-crosscompile-jetson-orin-agx-debug.cdx.json --spdx result-sbom-crosscompile-jetson-orin-agx-debug.spdx.json'
-                    sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#sbomnix -- .#packages.x86_64-linux.nvidia-jetson-orin-nx-debug-from-x86_64 --csv result-sbom-crosscompile-jetson-orin-nx-debug.csv --cdx result-sbom-crosscompile-jetson-orin-nx-debug.cdx.json --spdx result-sbom-crosscompile-jetson-orin-nx-debug.spdx.json'
-                    sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#sbomnix -- .#packages.x86_64-linux.generic-x86_64-debug  --csv result-sbom-generic-x86_64-debug.csv --cdx result-sbom-generic-x86_64-debug.cdx.json --spdx result-sbom-generic-x86_64-debug.spdx.json'
-                    sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#sbomnix -- .#packages.x86_64-linux.lenovo-x1-carbon-gen11-debug --csv result-sbom-lenovo-x1-carbon-gen11-debug.csv --cdx result-sbom-lenovo-x1-carbon-gen11-debug.cdx.json --spdx result-sbom-lenovo-x1-carbon-gen11-debug.spdx.json'
-                    sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#sbomnix -- .#packages.riscv64-linux.microchip-icicle-kit-debug --csv result-sbom-microchip-icicle-kit-debug.csv --cdx result-sbom-microchip-icicle-kit-debug.cdx.json --spdx result-sbom-microchip-icicle-kit-debug.spdx.json'
-                    sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#sbomnix -- .#packages.aarch64-linux.nvidia-jetson-orin-agx-debug --csv result-sbom-aarch64-jetson-orin-agx-debug.csv --cdx result-sbom-aarch64-jetson-orin-agx-debug.cdx.json --spdx result-sbom-aarch64-jetson-orin-agx-debug.spdx.json'
-                    sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#sbomnix -- .#packages.aarch64-linux.nvidia-jetson-orin-nx-debug --csv result-sbom-aarch64-jetson-orin-nx-debug.csv --cdx result-sbom-aarch64-jetson-orin-nx-debug.cdx.json --spdx result-sbom-aarch64-jetson-orin-nx-debug.spdx.json'
-                }
-            }
-        }
-    }
-    post {
-        always {
-            dir('ghaf') {
-                sh 'jq -s . result-provenance-*.json | jq ".[0] * .[1] * .[2] * .[3] * .[4] * .[5] * .[6]" > result-provenance.json'
-                archiveArtifacts artifacts: 'result-*', allowEmptyArchive: true
-            }
-        }
-        success {
-            script {
-                currentBuild.description = "Successfully built and archived artifacts"
-            }
-        }
-        failure {
-            script {
-                currentBuild.description = "Build failed"
-            }
-        }
-    }
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package com.homeaway.devtools.jenkins.testing
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ * Takes the place of a {@link GlobalVariable} in a {@link JenkinsPipelineSpecification} context.
+ * Intercepts every method call and forwards it to a mock for the current test-run.
+ * Mocks are named with the pattern <code>GlobalVariableName.methodName</code> - for example, in the following pipeline script under test:
+ * <pre><code>
+SomeVariable.someFunction("foo")
+ * </code></pre>
+ * The <b>someFunction</b> call could be verified by
+ * <pre><code>
+then:
+	1 * getPipelineMock("SomeVariable.someFunction")("foo")
+ * </code></pre>
+ * 
+ * @author awitt
+ *
+ */
+public class PipelineVariableImpersonator {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(PipelineVariableImpersonator.class)
+	
+	/**
+	 * The "Global Variable" property name to automatically generate mocks for.
+	 */
+	protected String for_property
+	
+	/**
+	 * The test suite that in which to automatically generate mocks.
+	 */
+	protected JenkinsPipelineSpecification for_spec
+	
+	/**
+	 * Create a stand-in for _property in the Spock _spec
+	 * @param _property The "Global Variable" property name to automatically generate mocks for.
+	 * @param _spec The test suite that in which to automatically generate mocks.
+	 */
+	public PipelineVariableImpersonator(String _property, JenkinsPipelineSpecification _spec) {
+		for_property = _property
+		for_spec = _spec
+	}
+	
+	/**
+	 * Intercept a missing method invocation on this "Global Variable" and try calling a mock with that name, instead.
+	 * 
+	 * @param _name The name of the method that didn't exist
+	 * @param _args The arguments to that method
+	 * 
+	 * @return The results of invoking {@link #for_spec}.getPipelineMock( "{@link #for_property}._name" )( _args )
+	 */
+	def methodMissing(String _name, _args) {
+		
+		def prepared_args = _args
+		
+		if( ! ( prepared_args instanceof Object[] ) ) {
+			// this method took one argument. Later on, unwrapping it will be WRONG, so, just pre-wrap it.
+			prepared_args = [_args].toArray()
+		}
+		
+		String key = "${for_property}.${_name}"
+		
+		Closure mock = for_spec.explicitlyMockPipelineStep( key, "(implicit-runtime) getPipelineMock(\"${key}\")" )
+
+		JenkinsPipelineSpecification.LOG_CALL_INTERCEPT(
+			"debug",
+			"method call on pipeline variable [${for_property}]",
+			this,
+			"methodMissing",
+			_name,
+			_args,
+			mock,
+			"call",
+			prepared_args,
+			true )
+		
+		// use the actual closure Mock
+		Object result = mock( *prepared_args )
+		
+		if( _args != null && _args.length >= 1 && _args[_args.length-1] instanceof Closure ) {
+			// there was at least one argument, and the last argument was a Closure
+			// this almost certainly means that the user's trying to pass the closure to the function as a "body"
+			// they probably want it to execute right now.
+			_args[_args.length-1]()
+		}
+		
+		return result
+	}
+	
+	def propertyMissing(String _name) {
+		return for_spec.getPipelineMock( "${for_property}.getProperty" )( _name )
+	}
+	
+	public String toString() {
+		return "Mock Generator for [${for_property}]"
+	}
 }
